fee1efaf7bed09811c85b23bc1a97229e0566f85
Jaime Montes de Oca <jaime0422@live.com>
2022-08-20 13:12:44 -0700

    Completed Assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,28 @@
 class CheesesController < ApplicationController
 
-  # GET /cheeses
-  def index
-    cheeses = Cheese.all
-    render json: cheeses
-  end
+    # GET /cheeses
+    def index
+        cheeses = Cheese.all
+        render json: cheeses
+    end
 
-  # GET /cheeses/:id
-  def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
+    # GET /cheeses/:id
+    def show
+        # cheese = Cheese.find_by(id: params[:id])
+        # render json: cheese
+
+        # cheese = Cheese.find_by(id: params[:id])
+        # render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+
+        cheese = Cheese.find_by(id: params[:id])
+        if cheese
+            render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+        else
+            # status: :not_found will produce a 404 status code
+            # it will not change how JSON data looks but send additional information to the client
+            # more symbols for status: can be found in Rails Guides
+            render json: { error: 'Cheese not found' }, status: :not_found
+        end
+    end
 
 end
